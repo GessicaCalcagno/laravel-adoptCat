6e5419c4f619c7327f45731ed910a1a82d4dc082
GessicaCalcagno <gessicacristinacalcagno@gmail.com>
2025-01-17 18:20:43 +0100

    Add: init + route/contatti

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
-});
+})->name("home");
+
+
+Route::get('/contatti', function () {
+    return view('contatti');
+})->name("contatti");
