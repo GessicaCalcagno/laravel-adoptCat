c891f6beb4fac0697bfca33916fc1446e4e91fde
GessicaCalcagno <gessicacristinacalcagno@gmail.com>
2025-01-18 17:54:57 +0100

    Add: test contact form

--- resources/views/contatti.blade.php
+++ resources/views/contatti.blade.php
@@ -1,5 +1,36 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Contatti</h1>
+    <div class="container mt-5">
+        <h1>Contattaci</h1>
+
+        @if (session('success'))
+            <div class="alert alert-success">
+                {{ session('success') }}
+            </div>
+        @endif
+
+        @if (session('error'))
+            <div class="alert alert-danger">
+                {{ session('error') }}
+            </div>
+        @endif
+
+        <form action="{{ route('contatti.submit') }}" method="POST">
+            @csrf
+            <div class="mb-3">
+                <label for="name" class="form-label">Nome</label>
+                <input type="text" class="form-control" id="name" name="name" required>
+            </div>
+            <div class="mb-3">
+                <label for="email" class="form-label">Email</label>
+                <input type="email" class="form-control" id="email" name="email" required>
+            </div>
+            <div class="mb-3">
+                <label for="message" class="form-label">Messaggio</label>
+                <textarea class="form-control" id="message" name="message" rows="4" ></textarea>
+            </div>
+            <button type="submit" class="btn btn-primary">Invia</button>
+        </form>
+    </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Mail;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,3 +22,25 @@ Route::get('/', function () {
 Route::get('/contatti', function () {
     return view('contatti');
 })->name("contatti");
+
+Route::post('/contatti', function () {
+    // Validazione dei dati
+    $data = request()->validate([
+        'name' => 'required',
+        'email' => 'required|email',
+        'message' => '',
+    ]);
+
+    // Invio dell'email
+    Mail::html("
+    <p><strong>Nome:</strong> {$data['name']}</p>
+    <p><strong>Email:</strong> {$data['email']}</p>
+    <p><strong>Messaggio:</strong> {$data['message']}</p>
+", function ($message) {
+        $message->to('test@adoptacat.com')->subject('Nuovo messaggio da Adopt a Cat');
+    });
+
+
+    // Reindirizzamento con messaggio di successo
+    return redirect('/contatti')->with('success', 'Messaggio inviato con successo!');
+})->name('contatti.submit');
